e893f35ae4725539f62488a3b66fc8f00591ff25
Angela Nyabuti <86233121+angelanyabuti@users.noreply.github.com>
2022-05-01 20:21:22 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const app =Vue.createApp({
+    template:'',
+    data(){
+        return{
+            firstName:'John',
+            lastName:'Doe',
+            email:'john@gmail.com',
+            gender:'male',
+            picture:'https://randomuser.me/api/portraits/men/30.jpg',
+            
+        }
+    },
+    methods: {
+        async getUser(){
+            const res = await fetch('https://randomuser.me/api')
+            //gives us the json data
+            const {results} = await res.json()
+           // console.log(results)
+            //this keyword is used to access data in return
+            this.firstName = results[0].name.first
+            this.lastName = results[0].name.last
+            this.email = results[0].email
+            this.gender = results[0].gender
+            this.picture = results[0].picture.large
+            
+        },
+    },
+})
+
+app.mount('#app')
